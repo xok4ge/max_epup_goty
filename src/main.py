c88693c3c57eb56d6ca082b0ea044283dd72af9c
xok4ge <glc500q@gmail.com>
2022-12-03 17:29:58 +0300

    йоу

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ class MainW(QMainWindow, design.Ui_MainWindow):
         self.img_line.setText(self.imgs)
 
     def work(self):
-        self.link = [self.link_1.text(), self.link_2.text(), self.link_3.text()]
+        self.link = [self.link_1.text(), self.link_2.text(), self.link_3.text(), self.link_4.text(), self.link_5.text(), self.link_6.text()]
         self.herz = self.herz_line.text()
         if self.directory and self.link and self.herz:
 
@@ -72,12 +72,19 @@ class MainW(QMainWindow, design.Ui_MainWindow):
 
                 # берем текст их полученной строки, меняем его на необходимый текст - получаем необходимую правильно стилизованную строку, остается расставить ее в нужные места
 
-            def chapter_to_str(chapter, links, herz):
+            def chapter_to_str(chapter, links, herz, sdvig):
                 try:
                     soup = bs4.BeautifulSoup(chapter, 'html.parser')
                     main_links = [bs4.BeautifulSoup(f'{link}', 'html.parser') if link else None for link in links]
                     main_links2 = list(filter(lambda x: x is not None, main_links))
-
+                    if sdvig < len(main_links2) and sdvig - 1 >= 0:
+                        for sd in range(sdvig):
+                            sdv = main_links2[sd]
+                            main_links2.remove(main_links2[sd])
+                            main_links2.append(sdv)
+                    else:
+                        sdvig = 0
+                    sdvig += 1
                     attrs, span, content, f_cont = {}, False, [], []
                     for tg in soup.body.contents:
                         content.append(str(tg))
@@ -102,13 +109,16 @@ class MainW(QMainWindow, design.Ui_MainWindow):
                             last = (len(content)) // herz
                             if last != 0:
                                 counter = 0
+                                kth = 0
                                 for i in range(a + 1, len(content) + 1, last):
-                                    if len(main_links2) - 1 >= counter:
-                                        content.insert(i, str(main_links2[counter]))
-                                        counter += 1
-                                    else:
-                                        counter = 0
-                                        content.insert(i, str(main_links2[0]))
+                                    if kth < herz:
+                                        if len(main_links2) - 1 >= counter:
+                                            content.insert(i, str(main_links2[counter]))
+                                            counter += 1
+                                        else:
+                                            counter = 0
+                                            content.insert(i, str(main_links2[0]))
+                                    kth += 1
                             else:
                                 content.insert(a + 1, str(main_links[0]))
                             truth = bs4.BeautifulSoup(str('\n'.join(content)), 'html.parser')
@@ -120,9 +130,10 @@ class MainW(QMainWindow, design.Ui_MainWindow):
                                         soup.span[f'{key}'] = val
                             else:
                                 soup.body.replace_with(truth)
-                    return str(soup)
+                    return str(soup), sdvig
                 except Exception as e:
-                    return None
+                    print(e)
+                    return None, 0
 
             a = os.listdir(str(self.directory))
             for el in a:
@@ -134,27 +145,45 @@ class MainW(QMainWindow, design.Ui_MainWindow):
             c = os.listdir('qwerty')
             for n in c:
                 book = f'qwerty\\{n}'
-                if self.imgs:
-                    if not os.path.exists(f'qwerty\{n}\OPS\images'):
-                        os.mkdir(f'qwerty\{n}\OPS')
-                        os.mkdir(f'qwerty\{n}\OPS\images')
-                    k = os.listdir(str(self.imgs))
-                    for el in k:
-                        new = shutil.copy(f'{str(self.imgs)}\{el}', f'qwerty\{n}\OPS\images')
-                        print(new)
+                for_img, metainf = None, None
                 items, files = [], []
                 for dirpath, _, filenames in os.walk(book):
                     for f in filenames:
                         items.append(os.path.abspath(os.path.join(dirpath, f)))
+                        if f == 'content.opf':
+                            for_img = os.path.abspath(dirpath)
+                            metainf = os.path.abspath(os.path.join(dirpath, f))
+                if self.imgs:
+                    k = os.listdir(str(self.imgs))
+                    with open(metainf, mode='r', encoding='utf-8') as f:
+                        a = f.read()
+                        soup = bs4.BeautifulSoup(a, 'html.parser')
+                        manifest = soup.find('manifest')
+                        new_otf = [str(qw) for qw in manifest.contents]
+                        for i in range(3):
+                            iline = f'<item id="1000{i}" href="{k[i]}" media-type="image/jpeg"/>'
+                            new_otf.append(iline)
+                        new_otf.append("</manifest>")
+                        new_otf.insert(0, '<manifest>')
+                        evo = bs4.BeautifulSoup(str('\n'.join(new_otf)), 'html.parser')
+                        manifest.replace_with(evo)
+                    with open(metainf, mode='w', encoding='utf-8') as f:
+                        f.write(str(soup))
+                    for el in k:
+                        new = shutil.copy(f'{str(self.imgs)}\{el}', for_img)
+                        print(new)
+
+
                 for elem in items:
                     name = os.path.splitext(elem)[1]
                     if name == '.html' or name == '.xhtml':
                         files.append(elem)
 
+                changer = 0
                 for file in files:
                         with open(file, mode='r', encoding='utf-8') as f:
                             a = f.read()
-                            html = chapter_to_str(a, self.link, int(self.herz))
+                            html, changer = chapter_to_str(a, self.link, int(self.herz), changer)
                             if html:
                                 with open(file, mode='w', encoding='utf-8') as f:
                                     f.write(html)
@@ -177,3 +206,4 @@ if __name__ == '__main__':
     ex = MainW()
     ex.show()
     sys.exit(app.exec_())
+
